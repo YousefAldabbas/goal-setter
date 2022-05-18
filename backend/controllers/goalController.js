8b3fd1f9bf4eb268fc50476f3f758ef65babd9be
yousefaldabbas0@gmail.com <yousefaldabbas0@gmail.com>
2022-05-18 03:08:41 +0300

    fix mongoDB connect issue

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,6 @@
 /* A middleware that wraps async functions to catch errors and pass them to the express error handler. */
 const asyncHandler = require("express-async-handler");
 const Goal = require("../models/goalModel");
-
 //@desc   Get goals
 //@route  GET /api/goals
 //@access Private
@@ -46,14 +45,12 @@ const updateGoal = asyncHandler(async (req, res) => {
 //@route  DELETE /api/goals/:id
 //@access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id);
-    if (!goal) {
-        res.status(404);
-        throw new Error("Goal not found");
-    }
-    await Goal.findByIdAndDelete(req.params.id);
-
-
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+  await Goal.findByIdAndDelete(req.params.id);
 
   res.status(200).json({ id: req.params.id });
 });
--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,14 @@ const app = express();
 const dotenv = require("dotenv").config();
 // it will allow us to use the env variables from file
 const port = process.env.PORT || 5000;
-const {errorHandler} = require("./middleware/errorMiddleware");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 connectDB();
 /* A middleware that allows us to use the body of the request. */
-app.use(express.json())
+app.use(express.json());
 /* A middleware that allows us to use the body of the request. */
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-
+app.use("/api/goals", require("./routes/goalRoutes"));
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
